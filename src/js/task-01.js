adb7cc5890b13d1170f4f2bb0c0583abcf6f8c67
Yura Mazhula <1orionset@gmail.com>
2021-10-18 15:26:01 +0300

    1

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,9 @@
 const itemsRef = document.querySelectorAll('.item');
 
 itemsRef.forEach(element => {
-    const categoriesLength = element.querySelectorAll('h2').length;
-    console.log(`Number of categories: ${categoriesLength}`);
     console.log(element.firstElementChild.textContent);
     console.log(element.lastElementChild.children.length);
     const title = element.querySelector('h2').innerHTML;
     const itemsLength = element.querySelectorAll('li').length;
-   });
+    console.log(`Категория: ${title} / Количество элементов: ${itemsLength}`);
+});
