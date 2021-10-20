48537f7d172fa165d94e7253abfca0afeccd1d17
Yura Mazhula <1orionset@gmail.com>
2021-10-20 15:06:06 +0300

    2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemsRef = ingredients.querySelectorAll('.item');
+
+itemsRef.forEach(element => {
+  const item = element.createElement("li");
+  item.element = "li";
+  item.elementList.add("item");
+    items.push(item);
+  }
+  IngredientsPalette.append(...items);
+}
