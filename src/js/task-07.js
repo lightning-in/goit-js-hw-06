c7641a43b47e2ca4015e3e34b40ef9f259115f26
Yura Mazhula <1orionset@gmail.com>
2021-10-26 20:35:26 +0300

    7

--- src/js/task-07.js
+++
@@ -1,6 +0,0 @@
-const inputRef = document.querySelector("#font-size-control");
-const inputRefMin = document.querySelector("#min");
-const inputRefMax = document.querySelector("#max");
-const inputText = document.querySelector("#text");
-
-
